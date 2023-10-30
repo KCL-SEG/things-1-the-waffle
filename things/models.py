bfa44140e2027a0d6af0baec0dde0415c728329f
the-waffle <43791700+the-waffle@users.noreply.github.com>
2023-10-30 15:37:34 +0000

    Update Thing model

--- things/models.py
+++ things/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.TextField(unique=True, blank=False, max_length=30)
+    description = models.TextField(unique=False, blank=True, max_length=120)
+    quantity = models.PositiveIntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
